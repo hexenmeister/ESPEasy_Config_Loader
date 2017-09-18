1d439e2f5bc5978d4af311b597c6b1f2618a693a
Alexander Schulz <git@s6z.de>
2017-09-19 00:08:55 +0200

    new test case

--- src/test/java/de/as/esptools/configloader/AllTests.java
+++ src/test/java/de/as/esptools/configloader/AllTests.java
@@ -4,8 +4,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import de.as.esptools.configloader.datatypes.NumericArrayItemTest;
+
 @RunWith(Suite.class)
-@SuiteClasses({ DataItemTest.class, ByteArrayItemTest.class, CharArrayItemTest.class })
+@SuiteClasses({ DataItemTest.class, ByteArrayItemTest.class, CharArrayItemTest.class, NumericArrayItemTest.class })
 public class AllTests {
 
 }
