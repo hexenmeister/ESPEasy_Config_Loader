f144f0e96b27b50e9a106816a4b44a80f695ea05
Alexander Schulz <git@s6z.de>
2017-09-24 00:15:35 +0200

    moved to new type

---
+++ src/main/java/de/as/esptools/configloader/datatypes/DataItemCreationException.java
@@ -0,0 +1,10 @@
+package de.as.esptools.configloader.datatypes;
+
+public class DataItemCreationException extends Exception {
+	private static final long serialVersionUID = 8320133839039042413L;
+
+	public DataItemCreationException(String msg) {
+		super(msg);
+	}
+
+}
