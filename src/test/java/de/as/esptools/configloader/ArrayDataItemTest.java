53c5ef4ca08152029d170678efc8c37be1e65bdf
Alexander Schulz <git@s6z.de>
2017-09-25 16:22:35 +0200

    implements more tests

--- src/test/java/de/as/esptools/configloader/ArrayDataItemTest.java
+++ src/test/java/de/as/esptools/configloader/ArrayDataItemTest.java
@@ -2,12 +2,15 @@ package de.as.esptools.configloader;
 
 import static org.junit.Assert.fail;
 
+import java.util.Arrays;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import de.as.esptools.configloader.datatypes.ArrayDataItem;
 import de.as.esptools.configloader.datatypes.ByteItem;
+import de.as.esptools.configloader.datatypes.DataImportException;
 import de.as.esptools.configloader.datatypes.DataItem;
 
 public class ArrayDataItemTest {
@@ -22,7 +25,13 @@ public class ArrayDataItemTest {
 
 		@Override
 		protected DataItem createType(byte[] data, int offset) {
-			return new ByteItem();
+			return new ByteItemTest(data, offset);
+		}
+
+		static class ByteItemTest extends ByteItem {
+			public ByteItemTest(byte[] data, int offset) {
+				super(data, offset);
+			}
 		}
 
 	}
@@ -33,13 +42,18 @@ public class ArrayDataItemTest {
 	}
 
 	@Test
+	public void testGetTypeName() {
+		Assert.assertEquals("ARRAY_TEST", inst.getTypeName());
+	}
+
+	@Test
 	public void testGetArrayLength() {
 		Assert.assertEquals(10, inst.getArrayLength());
 	}
 
 	@Test
-	public void testGetTypeName() {
-		Assert.assertEquals("ARRAY_TEST", inst.getTypeName());
+	public void testGetBytesPerItem() {
+		Assert.assertEquals(1, inst.getBytesPerItem());
 	}
 
 	@Test
@@ -48,53 +62,48 @@ public class ArrayDataItemTest {
 	}
 
 	@Test
-	public void testGetData() {
-		fail("Not yet implemented");
+	public void testImportBinOffset() throws DataImportException {
+		byte[] inBytes = { (byte) 0xFF, (byte) 0xDE, (byte) 0xAD, (byte) 0xF0, (byte) 0x0D, (byte) 0x00, (byte) 0x10,
+				(byte) 0x20, (byte) 0x30, (byte) 0x40, (byte) 0x50, (byte) 0xDE };
+		byte[] testBbytes = { (byte) 0xDE, (byte) 0xAD, (byte) 0xF0, (byte) 0x0D, (byte) 0x00, (byte) 0x10, (byte) 0x20,
+				(byte) 0x30, (byte) 0x40, (byte) 0x50 };
+
+		inst.importBin(inBytes, 1);
+		Assert.assertTrue(Arrays.equals(testBbytes, inst.exportBin()));
+
+		inst.importBin(inBytes, 0);
+		try {
+			Assert.assertTrue(Arrays.equals(testBbytes, inst.exportBin()));
+			fail("must be falsh");
+		} catch (AssertionError e) {
+			// NOP
+		}
 	}
 
 	@Test
-	public void testImportBinByteArray() {
-		fail("Not yet implemented");
+	public void testImportExportBin() throws DataImportException {
+		byte[] bytes = { (byte) 0xDE, (byte) 0xAD, (byte) 0xF0, (byte) 0x0D, (byte) 0x00, (byte) 0x10, (byte) 0x20,
+				(byte) 0x30, (byte) 0x40, (byte) 0x50 };
+		inst.importBin(bytes);
+		Assert.assertTrue(Arrays.equals(bytes, inst.exportBin()));
 	}
 
 	@Test
-	public void testImportBinByteArrayInt() {
-		fail("Not yet implemented");
+	public void testImportExportHex() throws DataImportException {
+		inst.importHex("DE AD F0 0D 00 10 20 30 40 50");
+		Assert.assertEquals("DE AD F0 0D 00 10 20 30 40 50", inst.exportHex());
 	}
 
 	@Test
-	public void testExportBin() {
-		fail("Not yet implemented");
+	public void testImportExportDataString() throws DataImportException {
+		inst.importDataString("DE AD F0 0D 00 10 20 30 40 50");
+		Assert.assertEquals("DE AD F0 0D 00 10 20 30 40 50", inst.exportDataString());
 	}
 
 	@Test
-	public void testImportHex() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testExportHex() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testImportDataString() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testExportDataString() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testExportTypeAndDataString() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetBytesPerItem() {
-		fail("Not yet implemented");
+	public void testExportTypeAndDataString() throws DataImportException {
+		inst.importDataString("DE AD F0 0D 00 10 20 30 40 50");
+		Assert.assertEquals("ARRAY_TEST[10] : DE AD F0 0D 00 10 20 30 40 50", inst.exportTypeAndDataString(false));
 	}
 
 }
