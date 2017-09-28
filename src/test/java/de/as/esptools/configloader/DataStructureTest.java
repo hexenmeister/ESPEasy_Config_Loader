bd2ce5d52c694c2e68607b8a7e0db0561f20af49
Alexander Schulz <schulzalex@gmx.de>
2017-09-28 13:31:30 +0200

    tests

--- src/test/java/de/as/esptools/configloader/DataStructureTest.java
+++ src/test/java/de/as/esptools/configloader/DataStructureTest.java
@@ -7,91 +7,106 @@ import org.junit.Test;
 
 import de.as.esptools.configloader.datatypes.BooleanItem;
 import de.as.esptools.configloader.datatypes.DataStructure;
+import de.as.esptools.configloader.datatypes.Int8Item;
+
 import org.junit.Assert;
 
 public class DataStructureTest {
 
-	private DataStructure struct;
+    private DataStructure createSmallStruct() {
+        DataStructure struct = new DataStructure("TEST_STRUCT");
+        struct.addItem(new BooleanItem());
 
-	@Before
-	public void setUp() throws Exception {
-		this.struct = new DataStructure("TEST_STRUCT");
-		this.struct.addItem(new BooleanItem());
-	}
+        return struct;
+    }
 
-	@Test
-	public void testDataStructure() {
-		fail("Not yet implemented");
-	}
+    @Before
+    public void setUp() throws Exception {
 
-	@Test
-	public void testAddItem() {
-		fail("Not yet implemented");
-	}
+    }
 
-	@Test
-	public void testGetItems() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testAddItemGetArrayLength() {
+        DataStructure struct = this.createSmallStruct();
+        Assert.assertEquals(1, struct.getArrayLength());
+        struct.addItem(new Int8Item(true));
+        Assert.assertEquals(2, struct.getArrayLength());
+    }
 
-	@Test
-	public void testGetItemCount() {
-		Assert.assertEquals(1, struct.getItemCount());
-	}
+    @Test
+    public void testGetItems() {
+        DataStructure struct = this.createSmallStruct();
+        Assert.assertEquals(1, struct.getItems().size());
+        struct.addItem(new Int8Item(true));
+        Assert.assertEquals(2, struct.getItems().size());
+    }
 
-	@Test
-	public void testGetTypeName() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testGetItemCount() {
+        DataStructure struct = this.createSmallStruct();
+        Assert.assertEquals(1, struct.getItemCount());
+        struct.addItem(new Int8Item(true));
+        Assert.assertEquals(1, struct.getItemCount());
+    }
 
-	@Test
-	public void testGetBinLength() {
-		Assert.assertEquals(1, struct.getItemCount());
-	}
+    @Test
+    public void testGetTypeName() {
+        DataStructure struct = this.createSmallStruct();
+        Assert.assertEquals("TEST_STRUCT", struct.getTypeName());
+    }
 
-	@Test
-	public void testImportBinByteArray() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testGetBinLength() {
+        Assert.assertEquals(1, this.createSmallStruct().getItemCount());
+        // TODO: more
+    }
 
-	@Test
-	public void testImportBinByteArrayInt() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testImportBinByteArray() {
+        // TODO
+        fail("Not yet implemented");
+    }
 
-	@Test
-	public void testImportHex() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testImportBinByteArrayInt() {
+        // TODO
+        fail("Not yet implemented");
+    }
 
-	@Test
-	public void testImportDataString() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testImportHex() {
+        // TODO
+        fail("Not yet implemented");
+    }
 
-	@Test
-	public void testExportBin() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testImportDataString() {
+        // TODO
+        fail("Not yet implemented");
+    }
 
-	@Test
-	public void testExportHex() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testExportBin() {
+        // TODO
+        fail("Not yet implemented");
+    }
 
-	@Test
-	public void testExportDataString() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testExportHex() {
+        // TODO
+        fail("Not yet implemented");
+    }
 
-	@Test
-	public void testExportTypeAndDataString() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testExportDataString() {
+        // TODO
+        fail("Not yet implemented");
+    }
 
-	@Test
-	public void testGetArrayLength() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testExportTypeAndDataString() {
+        // TODO
+        fail("Not yet implemented");
+    }
 
 }
