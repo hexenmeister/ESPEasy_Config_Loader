d0d76c5dd331df0bc76342df329327172d23f840
Alexander Schulz <schulzalex@gmx.de>
2017-10-04 10:57:26 +0200

    .

--- src/main/java/de/as/esptools/configloader/ConfigLoader.java
+++ src/main/java/de/as/esptools/configloader/ConfigLoader.java
@@ -3,12 +3,11 @@ package de.as.esptools.configloader;
 public class ConfigLoader {
 
 	public static void main(String[] args) {
-		// TODO implement
+		// TODO implement main
 
 	}
 
 	
-//	TODO:	Structure : importStructure(...)
 //	TODO:	DataItem/Structure(?) : importString()=> \HEX 00 FF 02 2D...
 //	TODO:	importString() => ignore comments: /* bla */ // bla
 
