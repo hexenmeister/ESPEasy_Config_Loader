3f24f4df358156c11de155e085a3b381c453178c
Alexander Schulz <schulzalex@gmx.de>
2017-10-25 15:28:57 +0200

    clean up

--- src/test/java/de/as/esptools/configloader/Int16ItemTest.java
+++ src/test/java/de/as/esptools/configloader/Int16ItemTest.java
@@ -1,332 +1,332 @@
 package de.as.esptools.configloader;
 
-import static org.junit.Assert.fail;
+import de.as.esptools.configloader.datatypes.DataImportException;
+import de.as.esptools.configloader.datatypes.Int16Item;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import de.as.esptools.configloader.datatypes.DataImportException;
-import de.as.esptools.configloader.datatypes.Int16Item;
+import static org.junit.Assert.fail;
 
 public class Int16ItemTest {
 
-	private Int16Item insts;
-	private Int16Item instu;
-
-	@Before
-	public void setUp() throws Exception {
-		insts = new Int16Item(true);
-		instu = new Int16Item(false);
-	}
-
-	@Test
-	public void testName() {
-		Assert.assertEquals("int16", insts.getTypeName());
-		Assert.assertEquals("uint16", instu.getTypeName());
-	}
-
-	@Test
-	public void testBinLength() {
-		Assert.assertEquals(2, insts.getBinLength());
-		Assert.assertEquals(2, instu.getBinLength());
-	}
-
-	@Test
-	public void testIsSigned() {
-		Assert.assertEquals(true, insts.isSigned());
-		Assert.assertEquals(false, instu.isSigned());
-	}
-
-	@Test
-	public void testTypeExportSigned() throws DataImportException {
-		insts.importDataString("0");
-		Assert.assertEquals("int16 : 0", insts.exportTypeAndDataString(false));
-
-		insts.importDataString("1");
-		Assert.assertEquals("int16 : 1", insts.exportTypeAndDataString(false));
-
-		insts.importDataString("-1");
-		Assert.assertEquals("int16 : -1", insts.exportTypeAndDataString(false));
-
-		insts.importDataString("127");
-		Assert.assertEquals("int16 : 127", insts.exportTypeAndDataString(false));
-
-		insts.importDataString("-128");
-		Assert.assertEquals("int16 : -128", insts.exportTypeAndDataString(false));
-
-		insts.importDataString("32767");
-		Assert.assertEquals("int16 : 32767", insts.exportTypeAndDataString(false));
-
-		insts.importDataString("-32768");
-		Assert.assertEquals("int16 : -32768", insts.exportTypeAndDataString(false));
-
-		try {
-			insts.importDataString("32768");
-			fail("invalid");
-		} catch (DataImportException e) {
-			// NOP
-		}
-
-		try {
-			insts.importDataString("-32769");
-			fail("invalid");
-		} catch (DataImportException e) {
-			// NOP
-		}
-	}
-
-	@Test
-	public void testTypeExportUnsigned() throws DataImportException {
-		instu.importDataString("0");
-		Assert.assertEquals("uint16 : 0", instu.exportTypeAndDataString(false));
-
-		instu.importDataString("1");
-		Assert.assertEquals("uint16 : 1", instu.exportTypeAndDataString(false));
-
-		try {
-			instu.importDataString("-1");
-			Assert.assertEquals("uint16 : -1", instu.exportTypeAndDataString(false));
-			fail("invalid");
-		} catch (DataImportException e) {
-			// NOP
-		}
-
-		instu.importDataString("127");
-		Assert.assertEquals("uint16 : 127", instu.exportTypeAndDataString(false));
-
-		try {
-			instu.importDataString("-128");
-			Assert.assertEquals("uint16 : -128", instu.exportTypeAndDataString(false));
-			fail("invalid");
-		} catch (DataImportException e) {
-			// NOP
-		}
-
-		instu.importDataString("65535");
-		Assert.assertEquals("uint16 : 65535", instu.exportTypeAndDataString(false));
-
-		try {
-			instu.importDataString("65536");
-			fail("invalid");
-		} catch (DataImportException e) {
-			// NOP
-		}
-	}
-
-	@Test
-	public void testImportStringSigned() throws DataImportException {
-		insts.importDataString("0");
-		Assert.assertEquals("0", insts.exportDataString());
-
-		insts.importDataString("-0");
-		Assert.assertEquals("0", insts.exportDataString());
-
-		insts.importDataString("1");
-		Assert.assertEquals("1", insts.exportDataString());
-
-		insts.importDataString("-1");
-		Assert.assertEquals("-1", insts.exportDataString());
-
-		insts.importDataString("127");
-		Assert.assertEquals("127", insts.exportDataString());
-
-		insts.importDataString("255");
-		Assert.assertEquals("255", insts.exportDataString());
-
-		insts.importDataString("-128");
-		Assert.assertEquals("-128", insts.exportDataString());
-
-		insts.importDataString("32767");
-		Assert.assertEquals("32767", insts.exportDataString());
-
-		insts.importDataString("-32768");
-		Assert.assertEquals("-32768", insts.exportDataString());
-
-		try {
-			insts.importDataString("32768");
-			fail("too big");
-		} catch (DataImportException e) {
-			// NOP
-		}
-		try {
-			insts.importDataString("-32769");
-			fail("too small");
-		} catch (DataImportException e) {
-			// NOP
-		}
-
-		try {
-			insts.importDataString("0.1");
-			fail("invalid");
-		} catch (DataImportException e) {
-			// NOP
-		}
-
-		try {
-			insts.importDataString("test");
-			fail("invalid");
-		} catch (DataImportException e) {
-			// NOP
-		}
-	}
-
-	@Test
-	public void testImportStringUnsigned() throws DataImportException {
-		instu.importDataString("0");
-		Assert.assertEquals("0", instu.exportDataString());
-
-		instu.importDataString("1");
-		Assert.assertEquals("1", instu.exportDataString());
-
-		instu.importDataString("127");
-		Assert.assertEquals("127", instu.exportDataString());
-
-		instu.importDataString("255");
-		Assert.assertEquals("255", instu.exportDataString());
-
-		instu.importDataString("65535");
-		Assert.assertEquals("65535", instu.exportDataString());
-
-		try {
-			instu.importDataString("65536");
-			fail("too big");
-		} catch (DataImportException e) {
-			// NOP
-		}
-
-		try {
-			instu.importDataString("-1");
-			fail("too small");
-		} catch (DataImportException e) {
-			// NOP
-		}
-
-		try {
-			instu.importDataString("0.1");
-			fail("invalid");
-		} catch (DataImportException e) {
-			// NOP
-		}
-
-		try {
-			instu.importDataString("test");
-			fail("invalid");
-		} catch (DataImportException e) {
-			// NOP
-		}
-	}
-
-	@Test
-	public void testExportStringSigned() throws DataImportException {
-		insts.setNumber(0);
-		Assert.assertEquals("0", insts.exportDataString());
-
-		insts.setNumber(1);
-		Assert.assertEquals("1", insts.exportDataString());
-
-		insts.setNumber(-1);
-		Assert.assertEquals("-1", insts.exportDataString());
-
-		insts.setNumber(127);
-		Assert.assertEquals("127", insts.exportDataString());
-
-		insts.setNumber(-128);
-		Assert.assertEquals("-128", insts.exportDataString());
-
-		insts.setNumber(32767);
-		Assert.assertEquals("32767", insts.exportDataString());
-
-		insts.setNumber(-32768);
-		Assert.assertEquals("-32768", insts.exportDataString());
-
-		try {
-			insts.setNumber(32768);
-			fail("invalid");
-		} catch (DataImportException e) {
-			// NOP
-		}
-
-		try {
-			insts.setNumber(-32769);
-			fail("invalid");
-		} catch (DataImportException e) {
-			// NOP
-		}
-	}
-
-	@Test
-	public void testExportStringUnsigned() throws DataImportException {
-		instu.setNumber(0);
-		Assert.assertEquals("0", instu.exportDataString());
-
-		instu.setNumber(1);
-		Assert.assertEquals("1", instu.exportDataString());
-
-		try {
-			instu.setNumber(-1);
-			fail("invalid");
-		} catch (DataImportException e) {
-			// NOP
-		}
-
-		instu.setNumber(127);
-		Assert.assertEquals("127", instu.exportDataString());
-
-		instu.setNumber(255);
-		Assert.assertEquals("255", instu.exportDataString());
-
-		instu.setNumber(65535);
-		Assert.assertEquals("65535", instu.exportDataString());
-
-		try {
-			instu.setNumber(65536);
-			fail("invalid");
-		} catch (DataImportException e) {
-			// NOP
-		}
-	}
-
-	@Test
-	public void testGetSetNumberSigned() throws DataImportException {
-		insts.setNumber(0);
-		Assert.assertEquals(0, insts.getNumber());
-
-		insts.setNumber(1);
-		Assert.assertEquals(1, insts.getNumber());
-
-		insts.setNumber(-1);
-		Assert.assertEquals(-1, insts.getNumber());
-
-		insts.setNumber(127);
-		Assert.assertEquals(127, insts.getNumber());
-
-		insts.setNumber(-128);
-		Assert.assertEquals(-128, insts.getNumber());
-
-		insts.setNumber(32767);
-		Assert.assertEquals(32767, insts.getNumber());
-
-		insts.setNumber(-32768);
-		Assert.assertEquals(-32768, insts.getNumber());
-	}
-
-	@Test
-	public void testGetSetNumberUnsigned() throws DataImportException {
-		instu.setNumber(0);
-		Assert.assertEquals(0, instu.getNumber());
-
-		instu.setNumber(1);
-		Assert.assertEquals(1, instu.getNumber());
-
-		instu.setNumber(127);
-		Assert.assertEquals(127, instu.getNumber());
-
-		instu.setNumber(255);
-		Assert.assertEquals(255, instu.getNumber());
-
-		instu.setNumber(65535);
-		Assert.assertEquals(65535, instu.getNumber());
-	}
+    private Int16Item insts;
+    private Int16Item instu;
+
+    @Before
+    public void setUp() throws Exception {
+        this.insts = new Int16Item(true);
+        this.instu = new Int16Item(false);
+    }
+
+    @Test
+    public void testName() {
+        Assert.assertEquals("int16", this.insts.getTypeName());
+        Assert.assertEquals("uint16", this.instu.getTypeName());
+    }
+
+    @Test
+    public void testBinLength() {
+        Assert.assertEquals(2, this.insts.getBinLength());
+        Assert.assertEquals(2, this.instu.getBinLength());
+    }
+
+    @Test
+    public void testIsSigned() {
+        Assert.assertEquals(true, this.insts.isSigned());
+        Assert.assertEquals(false, this.instu.isSigned());
+    }
+
+    @Test
+    public void testTypeExportSigned() throws DataImportException {
+        this.insts.importDataString("0");
+        Assert.assertEquals("int16 : 0", this.insts.exportTypeAndDataString(false));
+
+        this.insts.importDataString("1");
+        Assert.assertEquals("int16 : 1", this.insts.exportTypeAndDataString(false));
+
+        this.insts.importDataString("-1");
+        Assert.assertEquals("int16 : -1", this.insts.exportTypeAndDataString(false));
+
+        this.insts.importDataString("127");
+        Assert.assertEquals("int16 : 127", this.insts.exportTypeAndDataString(false));
+
+        this.insts.importDataString("-128");
+        Assert.assertEquals("int16 : -128", this.insts.exportTypeAndDataString(false));
+
+        this.insts.importDataString("32767");
+        Assert.assertEquals("int16 : 32767", this.insts.exportTypeAndDataString(false));
+
+        this.insts.importDataString("-32768");
+        Assert.assertEquals("int16 : -32768", this.insts.exportTypeAndDataString(false));
+
+        try {
+            this.insts.importDataString("32768");
+            fail("invalid");
+        } catch (DataImportException e) {
+            // NOP
+        }
+
+        try {
+            this.insts.importDataString("-32769");
+            fail("invalid");
+        } catch (DataImportException e) {
+            // NOP
+        }
+    }
+
+    @Test
+    public void testTypeExportUnsigned() throws DataImportException {
+        this.instu.importDataString("0");
+        Assert.assertEquals("uint16 : 0", this.instu.exportTypeAndDataString(false));
+
+        this.instu.importDataString("1");
+        Assert.assertEquals("uint16 : 1", this.instu.exportTypeAndDataString(false));
+
+        try {
+            this.instu.importDataString("-1");
+            Assert.assertEquals("uint16 : -1", this.instu.exportTypeAndDataString(false));
+            fail("invalid");
+        } catch (DataImportException e) {
+            // NOP
+        }
+
+        this.instu.importDataString("127");
+        Assert.assertEquals("uint16 : 127", this.instu.exportTypeAndDataString(false));
+
+        try {
+            this.instu.importDataString("-128");
+            Assert.assertEquals("uint16 : -128", this.instu.exportTypeAndDataString(false));
+            fail("invalid");
+        } catch (DataImportException e) {
+            // NOP
+        }
+
+        this.instu.importDataString("65535");
+        Assert.assertEquals("uint16 : 65535", this.instu.exportTypeAndDataString(false));
+
+        try {
+            this.instu.importDataString("65536");
+            fail("invalid");
+        } catch (DataImportException e) {
+            // NOP
+        }
+    }
+
+    @Test
+    public void testImportStringSigned() throws DataImportException {
+        this.insts.importDataString("0");
+        Assert.assertEquals("0", this.insts.exportDataString());
+
+        this.insts.importDataString("-0");
+        Assert.assertEquals("0", this.insts.exportDataString());
+
+        this.insts.importDataString("1");
+        Assert.assertEquals("1", this.insts.exportDataString());
+
+        this.insts.importDataString("-1");
+        Assert.assertEquals("-1", this.insts.exportDataString());
+
+        this.insts.importDataString("127");
+        Assert.assertEquals("127", this.insts.exportDataString());
+
+        this.insts.importDataString("255");
+        Assert.assertEquals("255", this.insts.exportDataString());
+
+        this.insts.importDataString("-128");
+        Assert.assertEquals("-128", this.insts.exportDataString());
+
+        this.insts.importDataString("32767");
+        Assert.assertEquals("32767", this.insts.exportDataString());
+
+        this.insts.importDataString("-32768");
+        Assert.assertEquals("-32768", this.insts.exportDataString());
+
+        try {
+            this.insts.importDataString("32768");
+            fail("too big");
+        } catch (DataImportException e) {
+            // NOP
+        }
+        try {
+            this.insts.importDataString("-32769");
+            fail("too small");
+        } catch (DataImportException e) {
+            // NOP
+        }
+
+        try {
+            this.insts.importDataString("0.1");
+            fail("invalid");
+        } catch (DataImportException e) {
+            // NOP
+        }
+
+        try {
+            this.insts.importDataString("test");
+            fail("invalid");
+        } catch (DataImportException e) {
+            // NOP
+        }
+    }
+
+    @Test
+    public void testImportStringUnsigned() throws DataImportException {
+        this.instu.importDataString("0");
+        Assert.assertEquals("0", this.instu.exportDataString());
+
+        this.instu.importDataString("1");
+        Assert.assertEquals("1", this.instu.exportDataString());
+
+        this.instu.importDataString("127");
+        Assert.assertEquals("127", this.instu.exportDataString());
+
+        this.instu.importDataString("255");
+        Assert.assertEquals("255", this.instu.exportDataString());
+
+        this.instu.importDataString("65535");
+        Assert.assertEquals("65535", this.instu.exportDataString());
+
+        try {
+            this.instu.importDataString("65536");
+            fail("too big");
+        } catch (DataImportException e) {
+            // NOP
+        }
+
+        try {
+            this.instu.importDataString("-1");
+            fail("too small");
+        } catch (DataImportException e) {
+            // NOP
+        }
+
+        try {
+            this.instu.importDataString("0.1");
+            fail("invalid");
+        } catch (DataImportException e) {
+            // NOP
+        }
+
+        try {
+            this.instu.importDataString("test");
+            fail("invalid");
+        } catch (DataImportException e) {
+            // NOP
+        }
+    }
+
+    @Test
+    public void testExportStringSigned() throws DataImportException {
+        this.insts.setNumber(0);
+        Assert.assertEquals("0", this.insts.exportDataString());
+
+        this.insts.setNumber(1);
+        Assert.assertEquals("1", this.insts.exportDataString());
+
+        this.insts.setNumber(-1);
+        Assert.assertEquals("-1", this.insts.exportDataString());
+
+        this.insts.setNumber(127);
+        Assert.assertEquals("127", this.insts.exportDataString());
+
+        this.insts.setNumber(-128);
+        Assert.assertEquals("-128", this.insts.exportDataString());
+
+        this.insts.setNumber(32767);
+        Assert.assertEquals("32767", this.insts.exportDataString());
+
+        this.insts.setNumber(-32768);
+        Assert.assertEquals("-32768", this.insts.exportDataString());
+
+        try {
+            this.insts.setNumber(32768);
+            fail("invalid");
+        } catch (DataImportException e) {
+            // NOP
+        }
+
+        try {
+            this.insts.setNumber(-32769);
+            fail("invalid");
+        } catch (DataImportException e) {
+            // NOP
+        }
+    }
+
+    @Test
+    public void testExportStringUnsigned() throws DataImportException {
+        this.instu.setNumber(0);
+        Assert.assertEquals("0", this.instu.exportDataString());
+
+        this.instu.setNumber(1);
+        Assert.assertEquals("1", this.instu.exportDataString());
+
+        try {
+            this.instu.setNumber(-1);
+            fail("invalid");
+        } catch (DataImportException e) {
+            // NOP
+        }
+
+        this.instu.setNumber(127);
+        Assert.assertEquals("127", this.instu.exportDataString());
+
+        this.instu.setNumber(255);
+        Assert.assertEquals("255", this.instu.exportDataString());
+
+        this.instu.setNumber(65535);
+        Assert.assertEquals("65535", this.instu.exportDataString());
+
+        try {
+            this.instu.setNumber(65536);
+            fail("invalid");
+        } catch (DataImportException e) {
+            // NOP
+        }
+    }
+
+    @Test
+    public void testGetSetNumberSigned() throws DataImportException {
+        this.insts.setNumber(0);
+        Assert.assertEquals(0, this.insts.getNumber());
+
+        this.insts.setNumber(1);
+        Assert.assertEquals(1, this.insts.getNumber());
+
+        this.insts.setNumber(-1);
+        Assert.assertEquals(-1, this.insts.getNumber());
+
+        this.insts.setNumber(127);
+        Assert.assertEquals(127, this.insts.getNumber());
+
+        this.insts.setNumber(-128);
+        Assert.assertEquals(-128, this.insts.getNumber());
+
+        this.insts.setNumber(32767);
+        Assert.assertEquals(32767, this.insts.getNumber());
+
+        this.insts.setNumber(-32768);
+        Assert.assertEquals(-32768, this.insts.getNumber());
+    }
+
+    @Test
+    public void testGetSetNumberUnsigned() throws DataImportException {
+        this.instu.setNumber(0);
+        Assert.assertEquals(0, this.instu.getNumber());
+
+        this.instu.setNumber(1);
+        Assert.assertEquals(1, this.instu.getNumber());
+
+        this.instu.setNumber(127);
+        Assert.assertEquals(127, this.instu.getNumber());
+
+        this.instu.setNumber(255);
+        Assert.assertEquals(255, this.instu.getNumber());
+
+        this.instu.setNumber(65535);
+        Assert.assertEquals(65535, this.instu.getNumber());
+    }
 
 }
